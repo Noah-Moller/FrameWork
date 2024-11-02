b94ececc689274e93cf2d783ab457832b417c940
Mini-Moller <noah@tetrix.tech>
2024-11-02 17:53:40 +1000

    Initial Commit

---
+++ FrameWork/FrameWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameWorkApp.swift
+//  FrameWork
+//
+//  Created by Noah Moller on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FrameWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrameWorkTests/FrameWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrameWorkTests.swift
+//  FrameWorkTests
+//
+//  Created by Noah Moller on 11/2/24.
+//
+
+import Testing
+@testable import FrameWork
+
+struct FrameWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
